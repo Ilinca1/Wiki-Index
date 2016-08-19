0dbeda5d0b43bfaff98748440ca69ff17000056a
ilinca1 <ilinca.vamesu@endava.com>
2016-08-19 15:57:46 +0300

    not important

--- CLONE/Wiki-Indexer-WikiIndexer/WikiIndexer/src/main/java/com/endava/project/repositories/OccurrenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.endava.project.repositories;
-
-import com.endava.project.entities.Occurrence;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OccurrenceRepository extends JpaRepository<Occurrence, Integer> {
-}
--- CLONE/Wiki-Indexer-WikiIndexer/WikiIndexer/src/main/java/com/endava/project/services/OccurrenceService.java
+++
@@ -1,9 +0,0 @@
-package com.endava.project.services;
-
-/**
- * Created by ivamesu on 8/11/2016.
- */
-public interface OccurrenceService {
-
-    void saveOccurrence(String title);
-}
